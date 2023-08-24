788d02a763835c20bc272a83d5a02d81eb86ae78
shamim-0 <dev.shamim0@gmail.com>
2023-08-25 02:19:47 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -118,126 +118,41 @@ function App() {
         <Route path="/reset" element={<Reset />}></Route>
         <Route path="/dashboard" element={<Dashboard />}>
           <Route path="/dashboard/users" element={<UserList />}></Route>
-          <Route
-            path="/dashboard/video-section"
-            element={<VideoSection />}
-          ></Route>
-          <Route
-            path="/dashboard/video-section/rutin"
-            element={<VideoDRutin />}
-          ></Route>
-          <Route
-            path="/dashboard/pdf-section"
-            element={<PDFDPDFSection />}
-          ></Route>
-          <Route
-            path="/dashboard/pdf-section/rutin"
-            element={<PDFDPDFRutine />}
-          ></Route>
-          <Route
-            path="/dashboard/job-solution-topically"
-            element={<JobSulationTopic />}
-          ></Route>
-          <Route
-            path="/dashboard/add-jobs-topic"
-            element={<JobsAddTopic />}
-          ></Route>
-          <Route
-            path="/dashboard/past-jobs-question"
-            element={<PastJobsQuestion />}
-          ></Route>
-          <Route
-            path="/dashboard/read-topicaly"
-            element={<ReadTopicaly />}
-          ></Route>
-          <Route
-            path="/dashboard/past-bcs-question"
-            element={<BCSPastQuestion />}
-          ></Route>
-          <Route
-            path="/dashboard/read-topic-add"
-            element={<TopicForReadTopic />}
-          ></Route>
-
-          {/* free weakly model test  */}
+          <Route path="/dashboard/video-section" element={<VideoSection />}></Route>
+          <Route path="/dashboard/video-section/rutin" element={<VideoDRutin />} ></Route>
+          <Route path="/dashboard/pdf-section" element={<PDFDPDFSection />}></Route>
+          <Route path="/dashboard/pdf-section/rutin" element={<PDFDPDFRutine />}></Route>
+          <Route path="/dashboard/job-solution-topically" element={<JobSulationTopic />} ></Route>
+          <Route path="/dashboard/add-jobs-topic" element={<JobsAddTopic />} ></Route>
+          <Route path="/dashboard/past-jobs-question" element={<PastJobsQuestion />}></Route>
+          <Route path="/dashboard/read-topicaly" element={<ReadTopicaly />}></Route>
+          <Route path="/dashboard/past-bcs-question" element={<BCSPastQuestion />} ></Route>
+          <Route path="/dashboard/read-topic-add" element={<TopicForReadTopic />}></Route>
           <Route path='/dashboard/free-weakly-modeltest' element={<DFreeWeaklyModelTest />}></Route>
           <Route path='/dashboard/free-weakly-modeltest/rutin' element={<FreeModelRutin />}></Route>
           <Route path='/dashboard/free-weakly-modeltest/exam-list' element={<FreeWeaklyExamList />}></Route>
-          <Route path="/dashboard/free-ewakly-examenee" element={<FreeWeaklyExamenee/>}></Route>
-          <Route path="/dashboard/free-weakly-examenee/:_id" element={<FreeWeaklyExamQuestion/>}></Route>
-          <Route
-            path="/dashboard/free-weakly-modeltest"
-            element={<DFreeWeaklyModelTest />}
-          ></Route>
+          <Route path="/dashboard/free-ewakly-examenee" element={<FreeWeaklyExamenee />}></Route>
+          <Route path="/dashboard/free-weakly-examenee/:_id" element={<FreeWeaklyExamQuestion />}></Route>
+          <Route path="/dashboard/free-weakly-modeltest" element={<DFreeWeaklyModelTest />} ></Route>
           <Route path="/dashboard/free-weakly-modeltest/rutin" element={<FreeModelRutin />}></Route>
-          {/* BCS For New */}
           <Route path="/dashboard/bcsForNew/routine" element={<BcsForNewRoutine />} />
-          <Route path="/dashboard/bcsForNew" element={<BcsForNewRoutineModelTest />}  />
-          {/* Job Solution */}
-          <Route
-            path="/dashboard/jobSolution"
-            element={<JobSolutionModelTest />}
-          />
-          <Route
-            path="/dashboard/jobSolution/routine"
-            element={<JobSolutionRotineD />}
-          />
-          {/* 9th - 20th grade preparation */}
-          <Route
-            path="/dashboard/gradePreparation"
-            element={<PreparationForGreadModelTest />}
-          />
-          <Route
-            path="/dashboard/gradePreparation/routine"
-            element={<PreparationForGreadRoutine />}
-          />
-          {/* Subject Wise preparation */}
-          <Route
-            path="/dashboard/subject-wise"
-            element={<SubjectWisePreparationModelTest />}
-          />
-          <Route
-            path="/dashboard/subject-wise/routine"
-            element={<SubjectWisePreparationRoutineD />}
-          />
-
-          {/* Bank preparation */}
-          <Route
-            path="/dashboard/bank-preparation"
-            element={<BankModelTest />}
-          />
-          <Route
-            path="/dashboard/bank-preparation/routine"
-            element={<BankRoutine />}
-          />
-
-          {/* Experience */}
-          <Route
-            path="/dashboard/experience"
-            element={<ExperienceModelTest />}
-          />
-          <Route
-            path="/dashboard/experience/routine"
-            element={<ExperienceRoutine />}
-          />
-
-          {/* Teacher Recruitment */}
-          <Route
-            path="/dashboard/teacherRecruitment"
-            element={<TeacherTestModel />}
-          />
-          <Route
-            path="/dashboard/teacherRecruitment/routine"
-            element={<TeacherRoutineD />}
-          />
-        </Route>
-
-        {/* Normal Route */}
+          <Route path="/dashboard/bcsForNew" element={<BcsForNewRoutineModelTest />} />
+          <Route path="/dashboard/jobSolution" element={<JobSolutionModelTest />} />
+          <Route path="/dashboard/jobSolution/routine" element={<JobSolutionRotineD />} />
+          <Route path="/dashboard/gradePreparation" element={<PreparationForGreadModelTest />} />
+          <Route path="/dashboard/gradePreparation/routine" element={<PreparationForGreadRoutine />} />
+          <Route path="/dashboard/subject-wise" element={<SubjectWisePreparationModelTest />} />
+          <Route path="/dashboard/subject-wise/routine" element={<SubjectWisePreparationRoutineD />} />
+          <Route path="/dashboard/bank-preparation" element={<BankModelTest />} />
+          <Route path="/dashboard/bank-preparation/routine" element={<BankRoutine />} />
+          <Route path="/dashboard/experience" element={<ExperienceModelTest />} />
+          <Route path="/dashboard/experience/routine" element={<ExperienceRoutine />} />
+          <Route path="/dashboard/teacherRecruitment" element={<TeacherTestModel />} />
+          <Route path="/dashboard/teacherRecruitment/routine" element={<TeacherRoutineD />} /> </Route>
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/sign-up" element={<SignUP />}></Route>
         <Route path="/reset" element={<Reset />}></Route>
-        {/* Video Segment Route */}
         <Route path="/video" element={<VideoVideo />} />
         <Route path="/video/videoroutine" element={<VideoRutine />} />
         <Route path="/video/videoweekly" element={<VideoWeekly />} />
@@ -246,14 +161,9 @@ function App() {
         <Route path="/video/videomath" element={<VideoMath />} />
         <Route path="/video/videoscience" element={<VideoScience />} />
         <Route path="/video/videoict" element={<VideoIct />} />
-        <Route
-          path="/video/videointernational"
-          element={<VideoInternational />}
-        />
+        <Route path="/video/videointernational" element={<VideoInternational />} />
         <Route path="/video/videobangladesh" element={<VideoBangladesh />} />
         <Route path="/video/videosuggestion" element={<VideoSuggetion />} />
-
-        {/* Pdf Segment Route */}
         <Route path="/pdf" element={<Pdf />} />
         <Route path="/pdf/pdfroutine" element={<PdfRutine />} />
         <Route path="/pdf/pdfweekly" element={<PdfWeekly />} />
@@ -265,12 +175,7 @@ function App() {
         <Route path="/pdf/pdfinternational" element={<PdfInternational />} />
         <Route path="/pdf/pdfbangladesh" element={<PdfBangladesh />} />
         <Route path="/pdf/pdfsuggestion" element={<PdfSuggetion />} />
-
-        {/* Topic Job Solution segment Route */}
-
         <Route path="/subjectwise" element={<SubjectWise />} />
-
-        {/* <=====================================Preminum Section ========================================> */}
         <Route path="/searchquestion" element={<Search />} />
         <Route path="/centralArchive" element={<CentralArchive />} />
         <Route path="/archive/:_id" element={<CenterlArchiveSingle />} />
@@ -279,72 +184,29 @@ function App() {
         <Route path="/centrul-result/:_id" element={<CenterlResultSingle />} />
         <Route path="/quizmaster" element={<QuizMaster />} />
         <Route path="/wrongandunanswered" element={<WrongAndUnanswered />} />
-
-        {/* read Topicaly  */}
         <Route path="/subjectwise/:subject" element={<SubjectWiseQuestion />} />
         <Route path="/subjectwise/topic/:subject" element={<AllTopic />} />
-        <Route
-          path="/TopicWiseReadQuestion/:topic"
-          element={<TopicWiseReadQuestion />}
-        />
-        <Route
-          path="/Topicread-topically/subtopic/:topic"
-          element={<AllSubTopic />}
-        />
-        <Route
-          path="/sub-topics-wise-question/:subtopic"
-          element={<SubTopicWiseQuestion />}
-        />
-
-        {/* <=====================================job sulation topic ========================================> */}
-        {/* /jobsulationtopic */}
+        <Route path="/TopicWiseReadQuestion/:topic" element={<TopicWiseReadQuestion />} />
+        <Route path="/Topicread-topically/subtopic/:topic" element={<AllSubTopic />} />
+        <Route path="/sub-topics-wise-question/:subtopic" element={<SubTopicWiseQuestion />} />
         <Route path="/jobsulationtopic" element={<JobSulation />} />
-        <Route
-          path="/jobs-question-subject/:subject"
-          element={<JobsSubjectWiseQuestion />}
-        />
+        <Route path="/jobs-question-subject/:subject" element={<JobsSubjectWiseQuestion />} />
         <Route path="/jobs-topic/:subject" element={<JobsTopic />} />
         <Route path="/jobs-subtopic/:topic" element={<JobsSubtopics />} />
-        <Route
-          path="/jobs-question-topic/:topic"
-          element={<JobsTopicQuestion />}
-        />
-        <Route
-          path="/jobs-question-subtopic/:subtopic"
-          element={<JobsSubQuestion />}
-        />
-
-        {/* privuse bcs  */}
-        {/* LastBCS */}
+        <Route path="/jobs-question-topic/:topic" element={<JobsTopicQuestion />} />
+        <Route path="/jobs-question-subtopic/:subtopic" element={<JobsSubQuestion />} />
         <Route path="/lastbcs" element={<LastBCS />} />
         <Route path="/lastbcs/:category" element={<LastBCSQuestion />} />
-
-        {/* job sulation past  */}
         <Route path="/jobsulationpast" element={<Jobsulationpast />} />
-
-        {/* Exam Section */}
-        <Route
-          path="/free-weakly-model-test"
-          element={<FreeWeeklyModelTest />}
-        />
+        <Route path="/free-weakly-model-test" element={<FreeWeeklyModelTest />} />
         <Route path="/preparationforgread" element={<PreparationForGread />} />
         <Route path="/jobsolution" element={<JobSolution />} />
         <Route path="/bcsfornew" element={<BCSForNew />} />
         <Route path="/bcsforexperience" element={<BCSForExperience />} />
         <Route path="/addteacher" element={<Teacher />} />
         <Route path="/addbank" element={<Bank />} />
-        <Route
-          path="/subjectwisepreparation"
-          element={<SubjectWisePreparation />}
-        />
-
-        <Route
-          path="/past-jobs-question/:category"
-          element={<JobSulationPastQuestion />}
-        />
-
-        {/* FreeWeaklyModelTest */}
-
+        <Route path="/subjectwisepreparation" element={<SubjectWisePreparation />} />
+        <Route path="/past-jobs-question/:category" element={<JobSulationPastQuestion />} />
         <Route path="/free-weakly-model-test" element={<FreeWeeklyModelTest />} />
         <Route path="/free-weakly-model-test/rutin" element={<FreeWeaklyRutin />} />
         <Route path="/free-weally-start-exam" element={<FreeWeaklyStartExam />} />
@@ -354,56 +216,17 @@ function App() {
         <Route path="/free-weakly-result/:_id" element={<FreeWeaklyResultSingleView />} />
         <Route path='/already-attent-exam' element={<AlreadyAttendExam />}></Route>
         <Route path='/free-weakly-meritlist-single/:date' element={<FreeWeaklyMeritListSingleView />}></Route>
-        <Route
-          path="/free-weakly-model-test"
-          element={<FreeWeeklyModelTest />}
-        />
-        <Route
-          path="/free-weakly-model-test/rutin"
-          element={<FreeWeaklyRutin />}
-        />
-        <Route
-          path="/free-weally-start-exam"
-          element={<FreeWeaklyStartExam />}
-        />
-        
-        <Route
-          path="/free-weakly-model-test/merit-list"
-          element={<FreeWeaklyMeritList />}
-        />
-
-        {/* BCS For New */}
-        <Route
-          path="/bcsfornew/bcsForNewRoutine"
-          element={<BCSForNewRoutineF />}
-        />
-        {/* Job Solution */}
-        <Route
-          path="/jobsolution/jobsolutionRoutine"
-          element={<JobSolutionRoutine />}
-        />
-        {/* Preparation For Grad */}
-        <Route
-          path="/preparationforgread/preparationforgreadroutine"
-          element={<PreparationForGreadRoutin />}
-        />
-        {/* Bank */}
+        <Route path="/free-weakly-model-test" element={<FreeWeeklyModelTest />} />
+        <Route path="/free-weakly-model-test/rutin" element={<FreeWeaklyRutin />} />
+        <Route path="/free-weally-start-exam" element={<FreeWeaklyStartExam />} />
+        <Route path="/free-weakly-model-test/merit-list" element={<FreeWeaklyMeritList />} />
+        <Route path="/bcsfornew/bcsForNewRoutine" element={<BCSForNewRoutineF />} />
+        <Route path="/jobsolution/jobsolutionRoutine" element={<JobSolutionRoutine />} />
+        <Route path="/preparationforgread/preparationforgreadroutine" element={<PreparationForGreadRoutin />} />
         <Route path="/addbank/addbankRoutine" element={<AddBankRoutine />} />
-        {/* BCS For Expericnce */}
-        <Route
-          path="/bcsforexperience/bcsforexperienceroutine"
-          element={<BCSForExperienceRoutine />}
-        />
-        {/* Subject Wise Preparation */}
-        <Route
-          path="/subjectwisepreparation/subjectwisepreparationRoutine"
-          element={<SubjectWisePreparationRoutine />}
-        />
-        {/* Teacher */}
-        <Route
-          path="/addteacher/addteacherRoutine"
-          element={<TeacherAddRoutine />}
-        />
+        <Route path="/bcsforexperience/bcsforexperienceroutine" element={<BCSForExperienceRoutine />} />
+        <Route path="/subjectwisepreparation/subjectwisepreparationRoutine" element={<SubjectWisePreparationRoutine />} />
+        <Route path="/addteacher/addteacherRoutine" element={<TeacherAddRoutine />} />
       </Routes>
       <ToastContainer />
     </div>
